e9a03dd272e2468a4bb325ab4907ab88f8904c93
IAmBOli <98320744+IAmBOli@users.noreply.github.com>
2022-10-26 00:07:21 -0400

    username mdp côté client

--- gitTp1/src/gitTp1/Client.java
+++ gitTp1/src/gitTp1/Client.java
@@ -20,12 +20,47 @@ public class Client {
 		socket = new Socket(serverAddress, serverPort);
 		System.out.format("The client is running %s: %d%n",serverAddress,serverPort);
 		
+		//RECUPERATION USERNAME CLIENT
 		Scanner inputUserName = new Scanner(System.in);
-        System.out.println("Username : ");
-        String userName = "";
-        userName = inputUserName.nextLine();
-        
-        Scanner inputImageName = new Scanner(System.in);
+        	System.out.println("Username : ");
+        	String userName = "";
+        	userName = inputUserName.nextLine();
+		
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		String helloMessageFromServer = in.readUTF();
+		System.out.println(helloMessageFromServer);
+		
+		//USERNAME MDP VERIFICATION
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		out.writeUTF(userName);
+
+
+		boolean MDPcorrect = false;
+		while(!MDPcorrect) {
+			byte messageType = in.readByte();
+			
+			switch(messageType) {
+			
+			case 1 :
+				Scanner inputMDP = new Scanner(System.in);
+		        System.out.println("Mot de passe : ");
+		        String MDP = "";
+		        MDP = inputMDP.nextLine();
+		        out.writeUTF(MDP);
+		        break;
+		        
+			case 2 :
+				MDPcorrect = true ;
+				break;
+				
+			case 3 :
+				String NouveauMDP = in.readUTF();
+				System.out.println("Votre mot de passe est :" + NouveauMDP);
+			}
+		}
+        	
+		//RECUPERATION IMAGE CLIENT
+        	Scanner inputImageName = new Scanner(System.in);
 		System.out.println("Entrer le nom de l'image à traiter (avec le .png) : ");
 		String imageName = "";
 		imageName = inputImageName.nextLine();
@@ -33,15 +68,10 @@ public class Client {
 		String pathToImage = System.getProperty("user.dir")+"/src/gitTp1/";
 		String path = pathToImage+imageName;
 		System.out.println(path);
+		
 		//chargement de l'image de base
 		BufferedImage img = ImageIO.read(new File(path));
-		
-		DataInputStream in = new DataInputStream(socket.getInputStream());
-		String helloMessageFromServer = in.readUTF();
-		System.out.println(helloMessageFromServer);
-		
-		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-		out.writeUTF(userName);
+
 		
 		ImageIO.write(img, "PNG", out); //envoi l'image
 		System.out.println("image envoyée au serveur");
